a70a5b3f6b8ed326a2760afd8fb83a2df9c36d27
Jason Joannou <jjoannou@bscglobal.com>
2025-07-16 17:15:52 +0200

    Update to crud

--- backend/db/crud.py
+++ backend/db/crud.py
@@ -1,7 +1,7 @@
 from models.config import allowed_models
 from firebase_admin import firestore
 
-def create_firebase_collection(db: firestore.client , collection_name: str, collection_id: str, collection_obj: allowed_models) -> str:
+async def create_firebase_collection(db: firestore.client , collection_name: str, collection_id: str, collection_obj: allowed_models) -> str:
     try:
         doc_ref = db.collection(collection_name).document(collection_id)
         doc_ref.set(collection_obj.model_dump())
@@ -9,9 +9,16 @@ def create_firebase_collection(db: firestore.client , collection_name: str, coll
     except Exception as e:
         raise Exception(f"Failed to create document: {e}")
     
-def update_firebase_collection(db: firestore.client, collection_name: str, collection_id: str, collection_obj: allowed_models) -> None:
+async def update_firebase_collection(db: firestore.client, collection_name: str, collection_id: str, collection_obj: allowed_models) -> None:
     try:
         doc_ref = db.collection(collection_name).document(collection_id)
         doc_ref.set(collection_obj.model_dump(), merge=True)
     except Exception as e:
         raise Exception(f"Failed to update document: {e}")
+    
+async def get_all_firebase_collections(db: firestore.client, collection_name: str) -> list[allowed_models]:
+    try:
+        docs = db.collection(collection_name).stream()
+        return [doc.to_dict() for doc in docs]
+    except Exception as e:
+        raise Exception(f"Failed to retrieve documents: {e}")
